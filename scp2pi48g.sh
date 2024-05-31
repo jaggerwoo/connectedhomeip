77f19223162a19de7381657edb844af40e138e5e
Woo <genggengwoo@gmail.com>
2024-05-31 11:34:01 +0800

    minor

--- scp2pi48g.sh
+++ scp2pi48g.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 dev_host=root@192.168.0.181
-target_root=~/connectedhomeip
+target_root=~/jaggerwoo_github/connectedhomeip
 
-scp ./examples/chip-tool/commands/interactive/InteractiveCommands.cpp $dev_host:$target_root/examples/chip-tool/commands/interactive/InteractiveCommands.cpp
-scp ./examples/chip-tool/commands/interactive/InteractiveCommands.h $dev_host:$target_root/examples/chip-tool/commands/interactive/InteractiveCommands.h
-scp ./examples/chip-tool/commands/pairing/PairingCommand.cpp $dev_host:$target_root/examples/chip-tool/commands/pairing/PairingCommand.cpp
-scp ./examples/chip-tool/commands/pairing/PairingCommand.h $dev_host:$target_root/examples/chip-tool/commands/pairing/PairingCommand.h
-scp ./examples/chip-tool/commands/clusters/ReportCommand.h $dev_host:$target_root/examples/chip-tool/commands/clusters/ReportCommand.h
-scp ./src/app/ReadClient.cpp $dev_host:$target_root/src/app/ReadClient.cpp
-scp ./src/app/ReadClient.h $dev_host:$target_root/src/app/ReadClient.h
-scp ./src/app/BufferedReadCallback.cpp $dev_host:$target_root/src/app/BufferedReadCallback.cpp
-scp ./src/app/BufferedReadCallback.h $dev_host:$target_root/src/app/BufferedReadCallback.h
-scp ./examples/common/websocket-server/WebSocketServer.cpp $dev_host:$target_root/examples/common/websocket-server/WebSocketServer.cpp
+# scp ./examples/chip-tool/commands/interactive/InteractiveCommands.cpp $dev_host:$target_root/examples/chip-tool/commands/interactive/InteractiveCommands.cpp
+# scp ./examples/chip-tool/commands/interactive/InteractiveCommands.h $dev_host:$target_root/examples/chip-tool/commands/interactive/InteractiveCommands.h
+# scp ./examples/chip-tool/commands/pairing/PairingCommand.cpp $dev_host:$target_root/examples/chip-tool/commands/pairing/PairingCommand.cpp
+# scp ./examples/chip-tool/commands/pairing/PairingCommand.h $dev_host:$target_root/examples/chip-tool/commands/pairing/PairingCommand.h
+# scp ./examples/chip-tool/commands/clusters/ReportCommand.h $dev_host:$target_root/examples/chip-tool/commands/clusters/ReportCommand.h
+# scp ./src/app/ReadClient.cpp $dev_host:$target_root/src/app/ReadClient.cpp
+# scp ./src/app/ReadClient.h $dev_host:$target_root/src/app/ReadClient.h
+# scp ./src/app/BufferedReadCallback.cpp $dev_host:$target_root/src/app/BufferedReadCallback.cpp
+# scp ./src/app/BufferedReadCallback.h $dev_host:$target_root/src/app/BufferedReadCallback.h
+# scp ./examples/common/websocket-server/WebSocketServer.cpp $dev_host:$target_root/examples/common/websocket-server/WebSocketServer.cpp
 
 ssh $dev_host "\
-	cd ~/connectedhomeip;\
+	cd ~/jaggerwoo_github/connectedhomeip;\
 	source scripts/activate.sh;\
 	./scripts/examples/gn_build_example.sh examples/chip-tool aaa;\
-	cp ~/connectedhomeip/aaa/chip-tool /usr/local/presen/pi/;"\
-	~/connectedhomeip/aaa/chip-tool interactive server --paa-trust-store-path /usr/local/etc/presen/chip_tool/credentials --storage-directory /usr/local/etc/presen/chip_tool/config;
+	cp ~/jaggerwoo_github/connectedhomeip/aaa/chip-tool /usr/local/presen/pi/;"\
+	~/jaggerwoo_github/connectedhomeip/aaa/chip-tool interactive server --paa-trust-store-path /usr/local/etc/presen/chip_tool/credentials --storage-directory /usr/local/etc/presen/chip_tool/config;
